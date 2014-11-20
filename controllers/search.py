ad00f01bfaca466fc25e2898a3c3932fc0d09b94
diogommartins <diogo.martins@unirio.br>
2014-11-19 22:35:47 -0200

    Mudanças no layout

--- controllers/search.py
+++ controllers/search.py
@@ -1,7 +1,12 @@
+# coding=utf-8
 from tables import ProjetosTable
 
 
 def index():
+    if not session.projetos:
+        session.flash = "Você precisa realizar uma busca."
+        redirect(URL('default', 'index'))
+
     table = ProjetosTable(session.projetos)
 
     return dict(
